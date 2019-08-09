32c443c2a3c832dc15f41102dc20cce137ab19e9
onyiny-ang <ltulloch@redhat.com>
2019-08-09 17:56:34 -0400

    Remove unused imports and files

--- src/icon-utils.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import {Uri} from 'vscode';
-
-const images= path.normalize('images');
-
-
-export function getIcon(filename: string): Uri {
-      return Uri.file(path.join(__dirname,'../', images, filename));
-}
