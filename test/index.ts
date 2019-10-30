ed0a26ac4870587bf61b9db78e634fe8e78297a3
Denis Golovin <dgolovin@redhat.com>
2019-10-30 15:01:06 -0700

    Restore source mapping for tests execution

--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-let mode: string = process.env.VSCOST_TEST_MODE || 'coverage';
+require('source-map-support').install();
 
+let mode: string = process.env.VSCOST_TEST_MODE || 'coverage';
 module.exports = require(`./index.${mode}`);
