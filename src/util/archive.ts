f71d116a046737c1b54788b69474d8c6494c9f5f
Sudhir Verma <sudhirvvermaa@gmail.com>
2019-12-03 17:09:23 +0530

    Fix review changes

--- src/util/archive.ts
+++ src/util/archive.ts
@@ -3,7 +3,10 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
+import * as fs from 'fs';
+import * as zlib from 'zlib';
 import targz = require('targz');
+import unzipm = require('unzip-stream');
 
 export class Archive {
     static unzip(zipFile: string, extractTo: string, prefix?: string): Promise<void> {
@@ -22,9 +25,31 @@ export class Archive {
                     err ? reject(err) : resolve();
                 });
             }
+            else if (zipFile.endsWith('.gz')) {
+                Archive.gunzip(zipFile, extractTo)
+                    .then(resolve)
+                    .catch(reject);
+            }
+            else if (zipFile.endsWith('.zip')) {
+                fs.createReadStream(zipFile)
+                    .pipe(unzipm.Extract({ path: extractTo }))
+                    .on('error', reject)
+                    .on('close', resolve);
+            }
             else {
                 reject(`Unsupported extension for '${zipFile}'`);
             }
+        });
+    }
+
+    static gunzip(source, destination): Promise<void> {
+        return new Promise((res, rej) => {
+            const src = fs.createReadStream(source);
+            const dest = fs.createWriteStream(destination);
+            src.pipe(zlib.createGunzip()).pipe(dest);
+            dest.on('close', res);
+            dest.on('error', rej);
+            src.on('error', rej);
         });
     }
 }
