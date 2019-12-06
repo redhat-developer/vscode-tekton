922c693b204b75a006a4f5454c26ec1d1b4f2cf1
Sudhir Verma <sudhirvvermaa@gmail.com>
2019-12-06 14:52:23 +0530

    'Tekton: Follow Log' command for PipelineRuns and TaskRuns

--- test/tekton/pipelinerun.test.ts
+++ test/tekton/pipelinerun.test.ts
@@ -116,6 +116,16 @@ suite('Tekton/PipelineRun', () => {
 
         });
 
+        suite('followLog', () => {
+
+            test('followLog calls the correct tkn command in terminal', async () => {
+                await PipelineRun.followLog(pipelinerunItem);
+
+                expect(termStub).calledOnceWith(Command.showPipelineRunFollowLogs(pipelinerunItem.getName()));
+            });
+
+        });
+
         suite('cancel', () => {
    
             setup(() => {
