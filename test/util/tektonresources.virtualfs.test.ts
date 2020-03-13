fe6f83bc0fbfd8d0022af4179e4f7a6a6cd1bad4
Yevhen Vydolob <yvydolob@redhat.com>
2020-03-13 15:46:00 +0200

    return back all tests
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- test/util/tektonresources.virtualfs.test.ts
+++ test/util/tektonresources.virtualfs.test.ts
@@ -121,127 +121,127 @@ suite('TektonResourceVirtualFileSystemProvider', () => {
     sandbox.restore();
   });
 
-  // test('should return error if kubectl is not available', async () => {
-  //   let isErr = false;
-  //   try {
-  //     await trvfsp.readFile(Uri.parse(tknUri));
-  //   } catch (err) {
-  //     expect(err).is.not.undefined;
-  //     isErr = true
-  //   }
+  test('should return error if kubectl is not available', async () => {
+    let isErr = false;
+    try {
+      await trvfsp.readFile(Uri.parse(tknUri));
+    } catch (err) {
+      expect(err).is.not.undefined;
+      isErr = true
+    }
     
-  //   expect(isErr).true;
-  // });
+    expect(isErr).true;
+  });
 
-  // test('should able to get yaml data from kubectl', async () => {
-  //   const api: k8s.API<k8s.KubectlV1> = {
-  //     available: true,
-  //     api: {
-  //       invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
-  //       portForward: sandbox.stub()
-  //     }
-  //   };
-  //   v1Stub.onFirstCall().value(api);
-  //   const result = await trvfsp.readFile(Uri.parse(tknUri));
-  //   expect(result.toString()).deep.equals(getYaml);
-  // });
+  test('should able to get yaml data from kubectl', async () => {
+    const api: k8s.API<k8s.KubectlV1> = {
+      available: true,
+      api: {
+        invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
+        portForward: sandbox.stub()
+      }
+    };
+    v1Stub.onFirstCall().value(api);
+    const result = await trvfsp.readFile(Uri.parse(tknUri));
+    expect(result.toString()).deep.equals(getYaml);
+  });
 
-  // test('throw error if command fails', async () => {
-  //   try {
-  //     await trvfsp.readFile(Uri.parse(tknUri));
-  //   } catch (err) {
-  //     expect(err.message).equals('kubectl is not available, check k8\'s documentation to install "kubectl"');
-  //   }
-  // });
+  test('throw error if command fails', async () => {
+    try {
+      await trvfsp.readFile(Uri.parse(tknUri));
+    } catch (err) {
+      expect(err.message).equals('kubectl is not available, check k8\'s documentation to install "kubectl"');
+    }
+  });
 
-  // test('should able to apply and save yaml data', async () => {
-  //   if (process.platform === 'win32') {
-  //     osStub.onFirstCall().returns('c:\\temp');
-  //   } else {
-  //     osStub.onFirstCall().returns('/temp');
-  //   }
-  //   const api: k8s.API<k8s.KubectlV1> = {
-  //     available: true,
-  //     api: {
-  //       invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
-  //       portForward: sandbox.stub()
-  //     }
-  //   };
-  //   v1Stub.value(api);
-  //   const content = await trvfsp.readFile(Uri.parse(tknUri));
-  //   await trvfsp.writeFile(Uri.parse(tknUri), content);
-  //   writeFileStub.calledOnce;
-  //   executeCommandStub.calledOnce;
-  //   openTextStub.calledOnce;
-  //   unlinkStub.calledOnce;
-  // });
+  test('should able to apply and save yaml data', async () => {
+    if (process.platform === 'win32') {
+      osStub.onFirstCall().returns('c:\\temp');
+    } else {
+      osStub.onFirstCall().returns('/temp');
+    }
+    const api: k8s.API<k8s.KubectlV1> = {
+      available: true,
+      api: {
+        invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
+        portForward: sandbox.stub()
+      }
+    };
+    v1Stub.value(api);
+    const content = await trvfsp.readFile(Uri.parse(tknUri));
+    await trvfsp.writeFile(Uri.parse(tknUri), content);
+    writeFileStub.calledOnce;
+    executeCommandStub.calledOnce;
+    openTextStub.calledOnce;
+    unlinkStub.calledOnce;
+  });
 
-  // test('return undefined if temp folder is not found', async () => {
-  //   const api: k8s.API<k8s.KubectlV1> = {
-  //     available: true,
-  //     api: {
-  //       invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
-  //       portForward: sandbox.stub()
-  //     }
-  //   };
-  //   v1Stub.value(api);
-  //   const content = await trvfsp.readFile(Uri.parse(tknUri));
-  //   if (process.platform === 'win32') {
-  //     osStub.onFirstCall().returns(undefined);
-  //   } else {
-  //     osStub.onFirstCall().returns(undefined);
-  //   }
-  //   const result = await trvfsp.writeFile(Uri.parse(tknUri), content);
-  //   expect(result).equals(undefined);
-  // });
+  test('return undefined if temp folder is not found', async () => {
+    const api: k8s.API<k8s.KubectlV1> = {
+      available: true,
+      api: {
+        invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
+        portForward: sandbox.stub()
+      }
+    };
+    v1Stub.value(api);
+    const content = await trvfsp.readFile(Uri.parse(tknUri));
+    if (process.platform === 'win32') {
+      osStub.onFirstCall().returns(undefined);
+    } else {
+      osStub.onFirstCall().returns(undefined);
+    }
+    const result = await trvfsp.writeFile(Uri.parse(tknUri), content);
+    expect(result).equals(undefined);
+  });
 
-  // test('throw error if command fails to update yaml file', async () => {
-  //   try {
-  //     const api: k8s.API<k8s.KubectlV1> = {
-  //       available: true,
-  //       api: {
-  //         invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
-  //         portForward: sandbox.stub()
-  //       }
-  //     };
-  //     v1Stub.value(api);
-  //     const content = await trvfsp.readFile(Uri.parse(tknUri));
-  //     if (process.platform === 'win32') {
-  //       osStub.onFirstCall().returns('c:\\temp');
-  //     } else {
-  //       osStub.onFirstCall().returns('/temp');
-  //     }
-  //     await trvfsp.writeFile(Uri.parse(tknUri), content);
-  //   } catch (err) {
-  //     expect(err.message).equals('error');
-  //   }
-  // });
+  test('throw error if command fails to update yaml file', async () => {
+    try {
+      const api: k8s.API<k8s.KubectlV1> = {
+        available: true,
+        api: {
+          invokeCommand: sandbox.stub().resolves({ stdout: getYaml, stderr: '', code: 0}),
+          portForward: sandbox.stub()
+        }
+      };
+      v1Stub.value(api);
+      const content = await trvfsp.readFile(Uri.parse(tknUri));
+      if (process.platform === 'win32') {
+        osStub.onFirstCall().returns('c:\\temp');
+      } else {
+        osStub.onFirstCall().returns('/temp');
+      }
+      await trvfsp.writeFile(Uri.parse(tknUri), content);
+    } catch (err) {
+      expect(err.message).equals('error');
+    }
+  });
 
-  // test('throw error if kubectl command fails to update yaml file', async () => {
-  //   try {
-  //     const api: k8s.API<k8s.KubectlV1> = {
-  //       available: true,
-  //       api: {
-  //         invokeCommand: sandbox.stub().resolves({ stdout: '', stderr: 'error', code: 0}),
-  //         portForward: sandbox.stub()
-  //       }
-  //     };
-  //     v1Stub.onSecondCall().value(api);
-  //     const content = await trvfsp.readFile(Uri.parse(tknUri));
-  //     if (process.platform === 'win32') {
-  //       osStub.onFirstCall().returns('c:\\temp');
-  //     } else {
-  //       osStub.onFirstCall().returns('/temp');
-  //     }
-  //     await trvfsp.writeFile(Uri.parse(tknUri), content);
-  //   } catch (err) {
-  //     expect(err).equals('error');
-  //   }
-  // });
+  test('throw error if kubectl command fails to update yaml file', async () => {
+    try {
+      const api: k8s.API<k8s.KubectlV1> = {
+        available: true,
+        api: {
+          invokeCommand: sandbox.stub().resolves({ stdout: '', stderr: 'error', code: 0}),
+          portForward: sandbox.stub()
+        }
+      };
+      v1Stub.onSecondCall().value(api);
+      const content = await trvfsp.readFile(Uri.parse(tknUri));
+      if (process.platform === 'win32') {
+        osStub.onFirstCall().returns('c:\\temp');
+      } else {
+        osStub.onFirstCall().returns('/temp');
+      }
+      await trvfsp.writeFile(Uri.parse(tknUri), content);
+    } catch (err) {
+      expect(err).equals('error');
+    }
+  });
 
-  // test('create file system uri parse', async () => {
-  //   workspaceFoldersStub.onFirstCall().resolves(wsFolder1);
-  //   const content = kubefsUri('pipeline/petclinic-deploy-pipeline', 'yaml');
-  //   expect(content).deep.equals(Uri.parse(`tekton://loadtektonresource/pipeline-petclinic-deploy-pipeline.yaml?value=pipeline/petclinic-deploy-pipeline&_=${nonce.Date().getTime()}`));
-  // });
+  test('create file system uri parse', async () => {
+    workspaceFoldersStub.onFirstCall().resolves(wsFolder1);
+    const content = kubefsUri('pipeline/petclinic-deploy-pipeline', 'yaml');
+    expect(content).deep.equals(Uri.parse(`tekton://loadtektonresource/pipeline-petclinic-deploy-pipeline.yaml?value=pipeline/petclinic-deploy-pipeline&_=${nonce.Date().getTime()}`));
+  });
 });
