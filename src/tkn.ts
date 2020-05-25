eeb96eafd76c42153ad936c525030d36e4c6d13d
Sudhir Verma <sudhirvvermaa@gmail.com>
2020-05-25 19:06:32 +0530

    show reason when pipelinerun pass or fail (#300)
    * show reason when pipelinerun pass or fail

--- src/tkn.ts
+++ src/tkn.ts
@@ -746,6 +746,7 @@ export class PipelineRun extends TektonNodeImpl {
   private started: string;
   private finished: string;
   private item: PipelineRunData;
+  private reason: string;
   constructor(parent: TektonNode,
     name: string,
     tkn: Tkn,
@@ -754,6 +755,7 @@ export class PipelineRun extends TektonNodeImpl {
     super(parent, name, ContextType.PIPELINERUN, tkn, collapsibleState, item.metadata.creationTimestamp, item.status ? item.status.conditions[0].status : '');
     this.started = item.metadata.creationTimestamp;
     this.finished = item.status?.completionTime;
+    this.reason = item.status?.conditions[0] ? `(${item.status?.conditions[0].reason})` : '';
     this.item = item;
   }
 
@@ -761,12 +763,16 @@ export class PipelineRun extends TektonNodeImpl {
     return this.name;
   }
 
+  get tooltip(): string {
+    return format(`${this.CONTEXT_DATA[this.contextValue].tooltip} ${this.reason}`, this);
+  }
+
   get description(): string {
     let r = '';
     if (this.finished) {
-      r = 'started ' + humanizer(Date.now() - Date.parse(this.started)) + ' ago, finished in ' + humanizer(Date.parse(this.finished) - Date.parse(this.started));
+      r = `started ${humanizer(Date.now() - Date.parse(this.started))} ago, finished in ${humanizer(Date.parse(this.finished) - Date.parse(this.started))}`;
     } else {
-      r = 'running for ' + humanizer(Date.now() - Date.parse(this.started));
+      r = `running for ${humanizer(Date.now() - Date.parse(this.started))}`;
     }
     return r;
   }
