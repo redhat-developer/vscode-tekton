4f0e6bc0a06d6091e852ab9bcb2a98791da9b105
Sudhir Verma <sudhirvvermaa@gmail.com>
2020-09-30 12:57:34 +0530

    Fix review changes

--- src/extension.ts
+++ src/extension.ts
@@ -28,7 +28,7 @@ import { TKN_RESOURCE_SCHEME, TKN_RESOURCE_SCHEME_READONLY, tektonVfsProvider } 
 import { updateTektonResource } from './tekton/deploy';
 import { deleteFromExplorer, deleteFromCustom } from './commands/delete';
 import { addTrigger } from './tekton/trigger';
-import { triggerSupport } from './util/triggerversion';
+import { TknVersion, version } from './util/tknversion';
 
 export let contextGlobalState: vscode.ExtensionContext;
 let k8sExplorer: k8s.ClusterExplorerV1 | undefined = undefined;
@@ -105,6 +105,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
   disposables.forEach((e) => context.subscriptions.push(e));
 
   detectTknCli();
+  triggerDetection();
 
   setCommandContext(CommandContext.TreeZenMode, false);
   setCommandContext(CommandContext.PipelinePreview, false);
@@ -152,18 +153,22 @@ async function isTekton(): Promise<boolean> {
 export function deactivate(): void {
 }
 
+async function triggerDetection(): Promise<void> {
+  setCommandContext(CommandContext.Trigger, false);
+  const tknVersionType: TknVersion = await version();
+  if (tknVersionType && tknVersionType.trigger !== 'unknown') {
+    setCommandContext(CommandContext.Trigger, true);
+  }
+}
+
 async function detectTknCli(): Promise<void> {
   setCommandContext(CommandContext.TknCli, false);
-  setCommandContext(CommandContext.Trigger, false);
 
   // start detecting 'tkn' on extension start
   const tknPath = await ToolsConfig.detectOrDownload();
-  const trigger = await triggerSupport();
+
   if (tknPath) {
     setCommandContext(CommandContext.TknCli, true);
-  }
-  if (trigger !== 'unknown' && trigger) {
-    setCommandContext(CommandContext.Trigger, true);
   }
 }
 
