f4c8e7b12388ab6894f44c58f381c89c5a67e9b4
Sudhir Verma <sudhirvvermaa@gmail.com>
2020-10-12 19:10:36 +0530

    Fix vulnerability (#425)

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,8 @@
-
 /*-----------------------------------------------------------------------------------------------
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
+
 'use strict';
 
 require('source-map-support').install();
