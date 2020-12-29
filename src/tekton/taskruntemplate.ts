decbc5332377509763c4ef414d416a8d8037490e
Sudhir Verma <sudhirvvermaa@gmail.com>
2020-12-29 15:02:12 +0530

    Fix review changes

--- src/tekton/taskruntemplate.ts
+++ src/tekton/taskruntemplate.ts
@@ -18,17 +18,15 @@ async function getTask(name: string): Promise<Task> {
 function getInputsOutputs(task: Task, taskRunTemplate: TaskRunTemplate): void {
   const resources = {};
   if (task.spec.resources?.outputs && task.spec.resources?.outputs.length !== 0) {
-    const outputs = [];
-    task.spec.resources.outputs.map((value) => {
-      outputs.push({name: value.name, resourceRef: {name: 'Change Me'}});
+    const outputs = task.spec.resources.outputs.map((value) => {
+      return {name: value.name, resourceRef: {name: 'Change Me'}};
     });
     resources['outputs'] = outputs;
   }
 
   if (task.spec.resources?.inputs && task.spec.resources?.inputs.length !== 0) {
-    const inputs = [];
-    task.spec.resources.inputs.map((value) => {
-      inputs.push({name: value.name, resourceRef: {name: 'Change Me'}});
+    const inputs = task.spec.resources.inputs.map((value) => {
+      return {name: value.name, resourceRef: {name: 'Change Me'}};
     });
     resources['inputs'] = inputs;
   }
@@ -36,20 +34,18 @@ function getInputsOutputs(task: Task, taskRunTemplate: TaskRunTemplate): void {
 }
 
 function workspace(task: Task, taskRunTemplate: TaskRunTemplate): void {
-  const workspacesContent = [];
   if (task.spec.workspaces && task.spec.workspaces.length !== 0) {
-    task.spec.workspaces.map(value => {
-      workspacesContent.push({name: value.name, emptyDir: {}});
+    const workspacesContent = task.spec.workspaces.map(value => {
+      return {name: value.name, emptyDir: {}};
     });
     taskRunTemplate.spec['workspaces'] = workspacesContent;
   }
 }
 
 function getParams(task: Task, taskRunTemplate: TaskRunTemplate): void {
-  const params = [];
   if (task.spec.params && task.spec.params.length !== 0) {
-    task.spec.params.map(value => {
-      params.push({name: value.name, value: 'Change Me'});
+    const params = task.spec.params.map(value => {
+      return {name: value.name, value: 'Change Me'};
     });
     taskRunTemplate.spec['params'] = params;
   }
