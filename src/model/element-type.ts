ad4ee61a1a6144f6497630007eef9a47e8a55644
Sudhir Verma <sudhirvvermaa@gmail.com>
2021-05-10 18:31:28 +0530

    Fix build issue

--- src/model/element-type.ts
+++ src/model/element-type.ts
@@ -1,4 +1,3 @@
-
 /*-----------------------------------------------------------------------------------------------
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
