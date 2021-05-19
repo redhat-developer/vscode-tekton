e280dbfae912470dec3822e9ee12060e14f37113
Sudhir Verma <sudhirvvermaa@gmail.com>
2021-05-19 15:19:00 +0530

    detect ipv6 address and added test

--- test/util/hideclusterinformation.test.ts
+++ test/util/hideclusterinformation.test.ts
@@ -7,15 +7,24 @@
 
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
-import { hidePath } from '../../src/util/hideclusterinformation';
+import { hideClusterInfo, hideIPAddress, hidePath } from '../../src/util/hideclusterinformation';
 
 const expect = chai.expect;
 chai.use(sinonChai);
 
 suite('Hide user information', () => {
 
+  const ipAddress = 'undefinedUnable to connect to the server: dial tcp 192.168.1.240:6443: connect: no route to host 192.168.1.240:6443 2001:0000:3238:DFE1:63::FEFB 2001:0:3238:DFE1:63::FEFB';
+
   const userPath = `undefinedUnable to connect to the server: error executing access token command "C:\\!Users\\test\\AppData\\Local\\test-code\\installer\\test-cloud-sdk\\bin\\test.cmd config config-helper --format=json": err=exec: "C:\\Users\test\\AppData\\Local\\test-code\\installer\\test-cloud-sdk\\₭ǝ℞ᖭ\\₭ǝ℞ᖭ.cmd": file does not exist output= stderr=
   "/₭℞ᖭ/apple/₭℞ᖭ/"      "./test/test" "test/test2" "test" `;
+
+  const hideClusterData = 'undefinedUnable to connect to the server: dial tcp 192.168.1.240:6443: connect: no route to host 192.168.1.240:6443 2001:0000:3238:DFE1:63::FEFB  https://test.com     connect to the server: dial tcp: lookup https://test.com:10.10.20.30: no such host';
+
+  test('hide cluster information', async () => {
+    const result = hideClusterInfo(hideClusterData);
+    expect(result).deep.equals('undefinedUnable to connect to the server: dial tcp IP Address :6443: connect: no route to host IP Address :6443 IPV6 Address   cluster info      connect to the server: dial tcp: lookup cluster info:  no such host');
+  });
 
   test('Hide local user path', async () => {
     const result = hidePath(userPath);
@@ -26,6 +35,17 @@ suite('Hide user information', () => {
   test('return string if local user path not found', async () => {
     const message = 'undefinedUnable to connect to the server: error executing access token command';
     const result = hidePath(message);
+    expect(result).deep.equals(message);
+  });
+
+  test('Hide user ipAddress', async () => {
+    const result = hideIPAddress(ipAddress);
+    expect(result).deep.equals('undefinedUnable to connect to the server: dial tcp IP Address :6443: connect: no route to host IP Address :6443 IPV6 Address  IPV6 Address ');
+  });
+
+  test('return string if user ipAddress not found', async () => {
+    const message = 'undefinedUnable to connect to the server: dial tcp connect: no route to host';
+    const result = hideIPAddress(message);
     expect(result).deep.equals(message);
   });
 });
