4bb9f8f98ed3680ee5bccbe137273a758b60c3fe
Sudhir Verma <sudhirvvermaa@gmail.com>
2022-11-03 18:11:33 +0530

    Fixing css

--- src/webview/bundle/form-components/FormInputText.tsx
+++ src/webview/bundle/form-components/FormInputText.tsx
@@ -6,19 +6,51 @@
 import * as React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { FormInputProps } from './FormInputProps';
+import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  overrides: {
+    MuiOutlinedInput: {
+      root: {
+        '& $notchedOutline': {
+          background: 'var(--vscode-input-background)',
+          borderColor: 'var(--vscode-contrastBorder)'
+        },
+        '&:hover $notchedOutline': {
+          background: 'var(--vscode-input-background)',
+          borderColor: 'var(--vscode-contrastBorder)'
+        },
+        '&$focused $notchedOutline': {
+          // borderColor: 'red'
+          background: 'var(--vscode-input-background)',
+          borderColor: 'var(--vscode-focusBorder)'
+        }
+      }
+    },
+    MuiInputLabel: {
+      outlined: {
+        color: 'var(--vscode-disabledForeground)',
+      }
+    }
+  }
+});
+
+
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function FormInputText({label, setValue }: FormInputProps) {
   return (
-    <TextField
-      fullWidth
-      required
-      placeholder='Use the schema registry/repository/image:version'
-      onChange={(text) => {
-        return setValue(text.target.value.trim());
-      }}
-      label={label}
-      variant="outlined"
-    />
+    <MuiThemeProvider theme={theme}>
+      <TextField
+        fullWidth
+        required
+        placeholder='Use the schema registry/repository/image:version'
+        onChange={(text) => {
+          return setValue(text.target.value.trim());
+        }}
+        label={label}
+        variant="outlined"
+      />
+    </MuiThemeProvider>
   );
 }
